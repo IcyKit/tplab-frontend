cffa7a352e7370b6fe108e31f9caff328060c6c7
IcyKitArch <icykitdesign@gmail.com>
2023-01-20 02:07:59 +0300

    Update: 50% of work finished

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,6 +1,6 @@
 import "./ProductCard.scss";
 import DiscountLabel from "../DiscountLabel";
-import Rating from '@mui/material/Rating';
+import Rating from "@mui/material/Rating";
 
 const ProductCard = () => {
   return (
@@ -16,14 +16,18 @@ const ProductCard = () => {
         <img src="https://4lapy.ru/resize/800x800/upload/iblock/714/7140b69c9d49dd89f1d35b1a99e27d18.jpg" />
         <div className="product-card__content_right">
           <h2>Сухой корм для взрослых собак мелких и карликовых пород</h2>
-          <div className='product-card__content_right-prices'>
-            <div className='product-card__content-right-prices_old-price'>
-              <h3>659<span>99</span></h3>
+          <div className="product-card__content_right-prices">
+            <div className="product-card__content-right-prices_old-price">
+              <h3>
+                659<span>99</span>
+              </h3>
               <p>СТАРАЯ ЦЕНА</p>
             </div>
-            <div className='product-card__content-right-prices_new-price'>
+            <div className="product-card__content-right-prices_new-price">
               <Rating value={4} />
-              <h3>359<span>99</span></h3>
+              <h3>
+                359<span>99</span>
+              </h3>
               <p>Цена по акции</p>
             </div>
           </div>
